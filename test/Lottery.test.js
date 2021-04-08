d1037bf62f9485f3e289edd0bbb672cb8121c438
Ashley Barr <ashley.barr@ar4s.com>
2021-04-08 12:09:18 -0600

    add test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -103,22 +103,25 @@ describe('Lottery Contract', () => {
       value: web3.utils.toWei('2', 'ether')
     });
 
+
+    // Account Balances
     const initialBalance = await web3.eth.getBalance(accounts[0]);
-
     await lottery.methods.pickWinner().send({ from: accounts[0] })
-    
     const finalBalance = await web3.eth.getBalance(accounts[0]);
-
     const difference = finalBalance - initialBalance;
 
     assert(difference > web3.utils.toWei('1.8', 'ether'));
 
     // Check the players array is cleared
-
     const players = await lottery.methods.getPlayers().call({
       from: accounts[0]
     });
 
     assert.strictEqual(0, players.length);
+
+    // Contract Balance 
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    
+    assert.strictEqual(0, Number(balance));
   })
 })
